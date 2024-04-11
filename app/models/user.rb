f16d907d6ffaf1edbef058845deba1246f24deaf
Jonathan Grinstead <jonathangrinstead.design@gmail.com>
2024-04-11 14:47:16 +0100

    feat: cloudinary set up and images added to user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: { case_sensitive: false }
+  has_one_attached :photo
 end
