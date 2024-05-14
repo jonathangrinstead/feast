2292b2ddc70b32b9764ade2017502b237769daaa
jonathangrinstead <jonathangrinstead.design@gmail.com>
2024-05-14 14:35:53 +0100

    feat: profile show page setup !

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,5 @@
 class ProfileController < ApplicationController
     def show
-        @user = User.find_by(username: params[:id])
+        @user = User.find_by(username: params[:username])
     end
 end
