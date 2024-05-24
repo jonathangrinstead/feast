bbc43cf9ea8056b74c873697ae52bbbe1f493578
jonathangrinstead <jonathangrinstead.design@gmail.com>
2024-05-24 14:50:11 +0100

    fix webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,21 +1,4 @@
 const { environment } = require('@rails/webpacker');
-const path = require('path');
-
-environment.config.merge({
-  entry: {
-    application: path.resolve(__dirname, '..', '..', 'app', 'javascript', 'stylesheets', 'application.css')
-  },
-  resolve: {
-    fallback: {
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "assert": require.resolve("assert"),
-      "http": require.resolve("stream-http"),
-      "https": require.resolve("https-browserify"),
-      "os": require.resolve("os-browserify/browser"),
-      "url": require.resolve("url")
-    }
-  }
-});
 
 module.exports = environment;
+
